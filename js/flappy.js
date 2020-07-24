1abfb48cb29ba2191cbf0dc94027fedfa4858b66
Lúcio Magno <devalmagno@gmail.com>
2020-07-24 19:04:27 -0300

    I made the layout of the project (Obstacles still without javascript only html)

--- js/flappy.js
+++ js/flappy.js
@@ -4,13 +4,7 @@ const img = document.createElement('img');
 
 img.src = './imgs/passaro.png'
 img.classList.add('passaro');
-img.style.width = '5%';
-img.style.position = 'absolute';
-img.style.top = '250px';
-img.style.left = '30%';
 
-button.onclick = e => {
-    button.style.display = 'none';
-}
+
 
 div.appendChild(img);
