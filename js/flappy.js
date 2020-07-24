ea55f0d29079752d031f3419bc5bc65734f3da0a
Lúcio Magno <devalmagno@gmail.com>
2020-07-24 16:34:10 -0300

    button event

--- js/flappy.js
+++ js/flappy.js
@@ -1,6 +1,16 @@
 const div = document.querySelector('[wm-flappy]');
-
+const button = document.querySelector('.start');
 const img = document.createElement('img');
+
 img.src = './imgs/passaro.png'
+img.classList.add('passaro');
+img.style.width = '5%';
+img.style.position = 'absolute';
+img.style.top = '250px';
+img.style.left = '30%';
+
+button.onclick = e => {
+    button.style.display = 'none';
+}
 
 div.appendChild(img);
