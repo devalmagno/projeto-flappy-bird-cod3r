1aa64bdd047300091136260f685119c5b5a624fb
Lúcio Magno <devalmagno@gmail.com>
2020-07-25 19:10:38 -0300

    I made a implement of random barriers in DOM.
    You going to love this. Press "F5" like a mad and have a new result (almost totally random) of barriers.

--- js/flappy.js
+++ js/flappy.js
@@ -1,10 +1,51 @@
-const div = document.querySelector('[wm-flappy]');
-const button = document.querySelector('.start');
-const img = document.createElement('img');
+function novoElemento(tagName, className) {
+    const elem = document.createElement(tagName);
+    elem.className = className; 
+    return elem;
+}
 
-img.src = './imgs/passaro.png'
-img.classList.add('passaro');
+function Barreira(reversa = false) {
+    this.elemento = novoElemento('div', 'barreira');
 
+    const borda = novoElemento('div', 'borda');
+    const corpo = novoElemento('div', 'corpo');
+    this.elemento.appendChild(reversa ? corpo : borda);
+    this.elemento.appendChild(reversa ? borda : corpo);
 
+    this.setAltura = altura => corpo.style.height = `${altura}px`;
+}
 
-div.appendChild(img);
+// const b = new Barreira(true);
+// b.setAltura(200);
+
+// document.querySelector('[wm-flappy]').appendChild(b.elemento);
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras');
+
+    this.superior = new Barreira(true);
+    this.inferior = new Barreira(false);
+
+    this.elemento.appendChild(this.superior.elemento);
+    this.elemento.appendChild(this.inferior.elemento);
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura);
+        const alturaInferior = altura - abertura - alturaSuperior;
+
+        this.superior.setAltura(alturaSuperior);
+        this.inferior.setAltura(alturaInferior);
+
+        console.log(alturaSuperior.toFixed(2), alturaInferior.toFixed(2));
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0]);
+    this.setX = x => this.elemento.style.left = `${x}px`;
+    this.largura = () => this.elemento.clientWidth;
+
+    this.sortearAbertura();
+    this.setX(x);
+}
+
+const b = new ParDeBarreiras(700, 200, 400);
+document.querySelector('[wm-flappy]').appendChild(b.elemento);
